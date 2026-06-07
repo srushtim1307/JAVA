88c93e9070baf0128addd1230525749c830817c2
srushtim1307 <smate2024@gmail.com>
2026-06-07 11:09:59 +0530

    Patterns

---
+++ Patterns/Practice7.java
@@ -0,0 +1,33 @@
+/*  *
+   ***
+  *****
+ *******
+*/
+
+import java.util.Scanner;
+class Practice7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            //space
+            for(int j = 0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j = 0 ; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            //space
+             for(int j = 0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
+
+
---
+++ Patterns/Practice8.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Practice8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            //space
+            for(int j = 0; j<i; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j = 0 ; j<(2*n-(2*i+1)); j++){
+                System.out.print("*");
+            }
+            //space
+             for(int j = 0; j<i; j++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
+
+

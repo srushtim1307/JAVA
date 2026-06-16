b49f14ffe270c94a939ae2b02aa964d061a4951c
srushtim1307 <smate2024@gmail.com>
2026-06-16 19:28:13 +0530

    Patterns

---
+++ Patterns/Practice9.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Practice9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            //space
+            for(int j = 0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j = 0 ; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            //space
+             for(int j = 0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+                for(int i = 0; i<n; i++){
+            //space
+            for(int j = 0; j<i; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j = 0 ; j<(2*n-(2*i+1)); j++){
+                System.out.print("*");
+            }
+            //space
+             for(int j = 0; j<i; j++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
+
---
+++ Patterns/Prctice10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Prctice10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<=2*n-1; i++){
+            int stars = i;
+            if(i>n){
+                stars = 2*n-i;
+            }
+            for(int j = 1; j<=stars; j++ ){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
